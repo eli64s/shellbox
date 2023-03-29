d2480575158690427272bc8f06d869bc3d214416
Eli <43382407+eli64s@users.noreply.github.com>
2023-03-29 16:16:05 -0500

    Create clean_py_project.sh

---
+++ clean_py_project.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Backup files
+find . -type f -name "*.py-e" -delete 
+
+# Clean up Python cache files
+find . -type f -name "*.DS_Store" -ls -delete
+find . -type f -name "*.py[co]" -delete
+find . -type d -name "__pycache__" -exec rm -rf {} +
+find src -type d -name "__pycache__" -exec rm -r {} \;
+
+# Remove build artifacts
+rm -rf build/ dist/ *.egg-info/
+
+# Remove Jupyter notebook checkpoints
+find . -type d -name ".ipynb_checkpoints" -exec rm -rf {} +
+
+# Remove pytest cache
+rm -rf .pytest_cache/
